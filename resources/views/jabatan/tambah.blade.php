dac765a8ae869ed0713d8ec5f7c3647a21dcc828
antonio <antoniozeyn@gmail.com>
2022-02-23 12:17:30 +0700

    merubah title

--- resources/views/jabatan/tambah.blade.php
+++ resources/views/jabatan/tambah.blade.php
@@ -6,7 +6,7 @@ body {
     background-image: url('https://th.bing.com/th/id/R.e3336101db5ae5745390d0e2993c91ee?rik=cEAETduhSOTmyQ&riu=http%3a%2f%2fwallpup.com%2fwp-content%2fuploads%2f2013%2f03%2fTechnology-Wallpaper-HD.jpg&ehk=73kpSd514htP1Xerv8aI21fxc0685GrPgoic9t6uvnA%3d&risl=&pid=ImgRaw&r=0');
 }
 </style>
-   <title>TAMBAH</title>
+   <title>TAMBAH DATA</title>
 </head>
 <body>
 <form action=" formulir.html" method="get">
